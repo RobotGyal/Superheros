8299492afe6692469a71ddb1f819ff60b5656a74
Aleia Knight <aleiaknight@gmail.com>
2019-09-21 16:44:07 -0700

    Completed second section

--- superheros.py
+++ superheros.py
@@ -9,28 +9,57 @@ class Ability:
 
 class Armor:
     def __init__(self, name, max_block):
-        pass
+        self.name = name
+        self.max_block = max_block
     def block(self):
-        pass
+        return random.randint(0, self.max_block)
 
 class Hero:
-    def __init__(self, name, starting_health):
-        self.starting_health = "100"
+    def __init__(self, name, starting_health=100):
+        self.abilities = []
+        self.armors = []
+        self.name = name
+        self.starting_health = starting_health
+        self.current_health = starting_health
+
     def add_ability(self, ability):
-        pass
+        self.abilities.append(ability)
+    def add_armor(self, armor):
+        self.armors.append(armor)
     def attack(self):
-        pass
-    def defend(self, incoming_danger):
-        pass
+        for ability in self.abilities:
+            total_damage=0
+            total_damage += ability.attack()
+        return total_damage
+    def defend(self, damage_amt):
+        for damage_amt in self.armors:
+            total_armor = 0
+            total_armor += damage_amt.block()
+        return total_armor
     def take_damage(self, damage):
-        pass
+        self.current_health -= damage
     def is_alive(self):
-        pass
+        if self.current_health > 0:
+            return  True
+        return False
     def fight(self, opponent):
-        pass
+        while (self.is_alive() == True and opponent.is_alive() == True):
+            opponent.take_damage(self.attack())
+            self.take_damage(opponent.attack())
+            if opponent.is_alive() == False:
+                print("Winner is " + self.name)
+            
 
 
 if __name__ == "__main__":
-    ability = Ability("Debugging ability", 20)
-    print(ability.name)
-    print(ability.attack())
+    hero1 = Hero("Wonder Woman")
+    opponent = Hero("Dumbledore")
+    ability1 = Ability("Super Speed", 300)
+    ability2 = Ability("Super Eyes", 130)
+    ability3 = Ability("Wizard Wand", 80)
+    ability4 = Ability("Wizard Beard", 20)
+    hero1.add_ability(ability1)
+    hero1.add_ability(ability2)
+    opponent.add_ability(ability3)
+    opponent.add_ability(ability4)
+    hero1.fight(opponent)
